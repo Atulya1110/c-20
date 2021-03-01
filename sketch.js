839fa830310bf6f294c4673f647fe82be9a5a65b
Atulya1110 <76092426+Atulya1110@users.noreply.github.com>
2021-03-01 17:06:20 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var wall,car 
+var speed, weight ;
+
+function setup() {
+  createCanvas(2000,2000);
+ car=createSprite(400, 200, 50, 50);
+ wall=createSprite(1500,200,70,height/20);
+ speed=random(55,90);
+ weight=random(400,1500);
+car.velocityx= speed;
+}
+
+function draw() {
+  background(255,255,255);  
+
+if(wall.x-car.x<(car.width+wall.width)/2);
+{
+  car.velocityX=0;
+  var deformation=0.5  * weight * speed * speed/22509;
+  if (deformation>180)
+  {
+    car.shapecolor=blue(225,0,0);
+  }
+  if (deformation<180 && deformation>100)
+  {
+    car.shapecolor= grey(230,230,0);
+  }
+  if (deformation<100)
+  {
+    car.shapecolor=red(0,225,0);
+  }
+}
+
+  drawSprites();
+}
